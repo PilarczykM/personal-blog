7f05f15ff2dfa06a2da3f3d774639875a52bf567
Marcin Pilarczyk <marcinpilarczyk@me.com>
2023-03-17 19:10:33 +0100

    Ignore dist

--- astro.config.js
+++ astro.config.js
@@ -7,5 +7,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://marcinpilarczyk.netlify.app',
   integrations: [mdx(), prefetch(), tailwind(), sitemap()],
 });
